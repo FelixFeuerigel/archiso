b25cd018a7491adaf2e2816982f122427450399a
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2023-03-06 00:54:43 +0100

    add installer

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="fefe-archlinux"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_application="FeFe Arch Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -21,5 +21,6 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/fefe-archstrap"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
