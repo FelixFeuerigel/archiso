5ae29b7c2ed3ada08e8f49cbee339a70ef958819
FelixFeuerigel <76600556+FelixFeuerigel@users.noreply.github.com>
2023-03-06 03:50:38 +0100

    include fefe-base package

--- profiledef.sh
+++ profiledef.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2034
 
 iso_name="fefe-archlinux"
-iso_label="ARCH_$(date +%Y%m)"
+iso_label="FEFE_ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="FeFe Arch Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
